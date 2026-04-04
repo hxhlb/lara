ecc1b8b5b80f5294da18d1d3665f82fcc31514dd
roooot <103732419+rooootdev@users.noreply.github.com>
2026-04-04 17:57:48 +0200

    👀

--- lara/lara-Bridging-Header.h
+++ lara/lara-Bridging-Header.h
@@ -4,17 +4,21 @@
 //
 
 #import <Foundation/Foundation.h>
+
 #import "darksword.h"
 #import "utils.h"
 #import "vfs.h"
 
 void test(NSString *path);
 
-bool setkernproc(NSString *path);
 bool dlkerncache(void);
+
 uint64_t getkernproc(void);
 uint64_t getrootvnode(void);
 uint64_t getprocsize(void);
+
 bool haskernproc(void);
+
 NSString *getkerncache(void);
+
 void clearkerncachedata(void);
